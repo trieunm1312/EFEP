cc831cb2af80c857927784aa6f641635dd5d380b
unknown <nguyenngoctram762@gmail.com>
2024-09-06 08:18:05 +0700

    init: first commit

---
+++ src/main/java/com/team1/efep/EfepApplication.java
@@ -0,0 +1,13 @@
+package com.team1.efep;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EfepApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EfepApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/team1/efep/EfepApplicationTests.java
@@ -0,0 +1,13 @@
+package com.team1.efep;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EfepApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
