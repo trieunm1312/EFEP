7e8a19b2abca03d254a0e5c8db4f6593708681a5
unknown <nguyenngoctram762@gmail.com>
2024-09-07 18:58:57 +0700

    feat: add 9 entity models, controllers, services, service implemetors, repositories, role

---
+++ src/main/java/com/team1/efep/service_implementors/BuyerServiceImpl.java
@@ -0,0 +1,10 @@
+package com.team1.efep.service_implementors;
+
+import com.team1.efep.services.BuyerService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class BuyerServiceImpl implements BuyerService {
+}
