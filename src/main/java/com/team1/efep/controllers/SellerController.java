0cf1c1fcaad0e8c65cb4a03b8ac117d7d700d016
unknown <nguyenngoctram762@gmail.com>
2024-09-09 20:01:00 +0700

    feat: add request models and response models, change controllers directory location

--- src/main/java/com/team1/efep/controllers/SellerController.java
+++ src/main/java/com/team1/efep/controllers/SellerController.java
@@ -1,4 +1,4 @@
-package com.team1.efep.enums.controllers;
+package com.team1.efep.controllers;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
@@ -6,5 +6,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequiredArgsConstructor
-public class PageController {
+@RequestMapping("/seller")
+public class SellerController {
 }
---
+++ src/main/java/com/team1/efep/models/request_models/AddToCartRequest.java
@@ -0,0 +1,4 @@
+package com.team1.efep.models.request_models;
+
+public class AddToCartRequest {
+}
