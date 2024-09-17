1977c3e6235febfdbfa0e623b72233d59789d1e0
XuanThang <nguyenxuanthang12012004@gmail.com>
2024-09-17 08:42:11 +0700

    feat: add view cart

--- src/main/resources/static/js/index.js
+++ src/main/resources/static/js/index.js
@@ -1,15 +1,5 @@
 const shopping = document.querySelector(".header__cart-wrap");
 const selectShopping = shopping.querySelector(".header__cart-icon");
-const icon = shopping.querySelector(".header__cart-icon");
-
-
-icon.addEventListener("mouseenter", () => {
-    icon.classList.add("hovered");
-});
-
-icon.addEventListener("mouseleave", () => {
-    icon.classList.remove("hovered");
-});
 
 selectShopping.addEventListener("click", () => {
     shopping.classList.toggle("active");
