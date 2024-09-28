1cc340e6f0fcc403c503ce9dda789b7bdc8603c9
unknown <nguyenngoctram762@gmail.com>
2024-09-28 19:46:56 +0700

    fix: fix view wishlist

--- src/main/java/com/team1/efep/EfepApplication.java
+++ src/main/java/com/team1/efep/EfepApplication.java
@@ -5,9 +5,11 @@ import com.team1.efep.enums.Status;
 import com.team1.efep.models.entity_models.Account;
 import com.team1.efep.models.entity_models.Seller;
 import com.team1.efep.models.entity_models.User;
+import com.team1.efep.models.entity_models.Wishlist;
 import com.team1.efep.repositories.AccountRepo;
 import com.team1.efep.repositories.SellerRepo;
 import com.team1.efep.repositories.UserRepo;
+import com.team1.efep.repositories.WishlistRepo;
 import lombok.RequiredArgsConstructor;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
@@ -21,6 +23,7 @@ public class EfepApplication {
     private final AccountRepo accountRepo;
     private final UserRepo userRepo;
     private final SellerRepo sellerRepo;
+    private final WishlistRepo wishlistRepo;
 
 
     public static void main(String[] args) {
@@ -54,6 +57,33 @@ public class EfepApplication {
                                                 .build())
                                 )
                                 .planPurchaseDate(null)
+                                .build()
+                );
+
+                User user1 = userRepo.save(
+                        User.builder()
+                                .account(
+                                        accountRepo.save(Account.builder()
+                                                .email("buyer1@efep.com")
+                                                .status(Status.ACCOUNT_STATUS_ACTIVE)
+                                                .password("123")
+                                                .role(Role.BUYER)
+                                                .build())
+                                )
+                                .name("buyer1")
+                                .phone("")
+                                .avatar("")
+                                .background("")
+                                .orderList(null)
+                                .wishlist(null)
+                                .seller(null)
+                                .build()
+                );
+
+                wishlistRepo.save(
+                        Wishlist.builder()
+                                .wishlistItemList(null)
+                                .user(user1)
                                 .build()
                 );
 
