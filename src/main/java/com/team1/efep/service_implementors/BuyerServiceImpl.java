54a2390cb2497559527dd053625a8d2e45054e11
unknown <nguyenngoctram762@gmail.com>
2024-10-06 08:08:25 +0700

    fix: add to wish list

--- src/main/java/com/team1/efep/service_implementors/BuyerServiceImpl.java
+++ src/main/java/com/team1/efep/service_implementors/BuyerServiceImpl.java
@@ -168,7 +168,7 @@ public class BuyerServiceImpl implements BuyerService {
         if (checkExistedItem(request, wishlist)) {
             WishlistItem wishlistItem = wishlistItemRepo.findByFlower_Id(request.getFlowerId()).orElse(null);
             assert wishlistItem != null;
-            wishlistItem.setQuantity(wishlistItem.getQuantity() + request.getQuantity());
+            wishlistItem.setQuantity(wishlistItem.getQuantity() + 1);
             wishlistItemRepo.save(wishlistItem);
         } else {
             wishlist.getWishlistItemList().add(
@@ -176,7 +176,7 @@ public class BuyerServiceImpl implements BuyerService {
                             WishlistItem.builder()
                                     .wishlist(wishlist)
                                     .flower(flower)
-                                    .quantity(request.getQuantity())
+                                    .quantity(1)
                                     .build()));
             accountRepo.save(account);
         }
