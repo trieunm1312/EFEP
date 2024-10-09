b57956400b5da61f64c20e1844731df26ecd8d52
unknown <vannhuquynhp@gmail.com>
2024-10-09 17:59:40 +0700

    feat: add validation for login, renewPass, changePass, register

--- src/main/java/com/team1/efep/validations/ChangePasswordValidation.java
+++ src/main/java/com/team1/efep/validations/ChangePasswordValidation.java
@@ -1,5 +1,6 @@
 package com.team1.efep.validations;
 
+import com.team1.efep.configurations.MapConfig;
 import com.team1.efep.models.request_models.ChangePasswordRequest;
 
 import java.util.HashMap;
@@ -9,12 +10,20 @@ public class ChangePasswordValidation {
     public static Map<String, String> validate(ChangePasswordRequest request) {
         Map<String, String> errors = new HashMap<String, String>();
 
+        if(request.getCurrentPassword().isEmpty()) {
+            return MapConfig.buildMapKey(errors,"Current password cannot be empty");
+        }
+
+        if(request.getNewPassword().isEmpty()) {
+            return MapConfig.buildMapKey(errors,"New password cannot be empty");
+        }
+
         if(request.getNewPassword().equals(request.getCurrentPassword())) {
-            errors.put("error", "New password must be different from the current password.");
+           return MapConfig.buildMapKey(errors,"New password must be different from the current password");
         }
 
         if(!request.getNewPassword().equals(request.getConfirmPassword())) {
-            errors.put("error", "New password and confirm password do not match.");
+            return MapConfig.buildMapKey(errors, "New password and confirm password do not match");
         }
         // code validate here
         return errors;
--- src/main/java/com/team1/efep/validations/RegisterValidation.java
+++ src/main/java/com/team1/efep/validations/RegisterValidation.java
@@ -8,32 +8,62 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class RegisterValidation {
-    public static Map<String, String> validate(RegisterRequest request) {
+
+    public static Map<String, String> validate(RegisterRequest request, AccountRepo accountRepo) {
         Map<String, String> errors = new HashMap<>();
         //code validate here
 
         // name isn't duplicated
-        // --> nam is duplicated
-//        if(request.getName() != null) {
-//            MapConfig.buildMapKey(errors, "Name is duplicated");
-//        }
-//
-//        // phone must be 10-digit number, isn't duplicated
-//        // --> phone must not 10-digit number, is duplicated
-//        if(request.getPhone().isBlank()) {
-//            MapConfig.buildMapKey(errors, "Phone is duplicated");
-//        }
-//
-//        // email invalid format email
-//        // email invalid format email
-//
-//
-//        //password is duplicated confirm password
-//        //password isn't duplicated cofirm password
-//        if(!request.getPassword().equals(request.getConfirmPassword())) {
-//            MapConfig.buildMapKey(errors, "Password isn't matched");
-//        }
+        // --> name is duplicated
+        if (request.getName().isEmpty()) {
+            return MapConfig.buildMapKey(errors, "Name cannot be empty");
+        }
 
+        if (accountRepo.findByName(request.getName()).isPresent()) {
+            return MapConfig.buildMapKey(errors, "Name already exists");
+        }
+
+        // phone must be 10-digit number, isn't duplicated
+        // --> phone must not 10-digit number, is duplicated
+        if (request.getPhone().isEmpty()) {
+            return MapConfig.buildMapKey(errors, "Phone cannot be empty");
+        }
+
+        if (accountRepo.findByPhone(request.getPhone()).isPresent()) {
+            return MapConfig.buildMapKey(errors, "Phone already exists");
+        }
+
+        if (!request.getPhone().matches("\\d{10}")) {
+            return MapConfig.buildMapKey(errors, "Phone number must be 10 digits");
+        }
+
+        // email invalid format email
+        if (request.getEmail().isEmpty()) {
+            return MapConfig.buildMapKey(errors, "Email cannot be empty");
+        }
+
+        if (!request.getEmail().matches("^[A-Za-z0-9+_.-]+@[A-Za-z0-9.-]+$")) {
+            return MapConfig.buildMapKey(errors, "Email is in invalid format");
+        }
+
+        if (accountRepo.findByEmail(request.getEmail()).isPresent()) {
+            return MapConfig.buildMapKey(errors, "Email already exists");
+        }
+
+        //password is equal confirmed password
+        //password isn't equal confirmed password
+        if (request.getPassword().isEmpty()) {
+            return MapConfig.buildMapKey(errors, "Password cannot be empty");
+        }
+
+        if (!request.getPassword().matches("^(?=.*[A-Za-z])(?=.*\\d)(?=.*[@$!%*#?&])[A-Za-z\\d@$!%*#?&]{8,}$")) {
+            return MapConfig.buildMapKey(errors, "Password is in invalid format");
+        }
+
+        if (!request.getPassword().equals(request.getConfirmPassword())) {
+            return MapConfig.buildMapKey(errors, "Confirmed password doesn't match the password");
+
+        }
 
         return errors;
     }
