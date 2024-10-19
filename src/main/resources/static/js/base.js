67ad715bd6682c8ad1d242dc037b96918b8486a1
unknown <nguyenngoctram762@gmail.com>
2024-10-19 18:23:46 +0700

    fix checkout out wish list

--- src/main/resources/static/js/base.js
+++ src/main/resources/static/js/base.js
@@ -158,10 +158,13 @@ function handleCheckout() {
     const paymentMethod = document.getElementById('paymentMethod').value;
     const checkoutForm = document.getElementById('checkout');
 
+    const codActionUrl = checkoutForm.getAttribute('data-cod-action');
+    const vnpayActionUrl = checkoutForm.getAttribute('data-vnpay-action');
+
     if (paymentMethod === 'COD') {
-        checkoutForm.setAttribute('action', checkoutForm.getAttribute('data-cod-action'));
+        checkoutForm.setAttribute('action', codActionUrl);
     } else if (paymentMethod === 'VNPay') {
-        checkoutForm.setAttribute('action', checkoutForm.getAttribute('data-vnpay-action'));
+        checkoutForm.setAttribute('action', vnpayActionUrl);
     }
     return true;
 }
