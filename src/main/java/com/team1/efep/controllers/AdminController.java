d94612e22ba79410e3a55a7ed1e4c00c0a1e78da
NeoNgyn <nguyenphucnhan2004@gmail.com>
2024-10-28 10:01:15 +0700

    fix: fix admin pages header + sidebar, delete: delete dailyUser chart + compare sold category chart

--- src/main/java/com/team1/efep/controllers/AdminController.java
+++ src/main/java/com/team1/efep/controllers/AdminController.java
@@ -11,8 +11,8 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
-@RestController
-//@Controller
+//@RestController
+@Controller
 @RequiredArgsConstructor
 @RequestMapping("/admin")
 @Tag(name = "Admin")
