d491af0cd78cae0b46e5ef97aeb2ad11de929c72
trieu <nguyenngoctram762@gmail.com>
2024-10-28 11:23:48 +0700

    fix: fix date format in get Orders In Month

--- src/main/java/com/team1/efep/models/entity_models/Category.java
+++ src/main/java/com/team1/efep/models/entity_models/Category.java
@@ -19,7 +19,7 @@ public class Category {
 
     private String name;
 
-    @OneToMany(mappedBy = "category")
+    @OneToMany(mappedBy = "category", fetch = FetchType.EAGER)
     @ToString.Exclude
     @EqualsAndHashCode.Exclude
     private List<FlowerCategory> flowerCategoryList;
